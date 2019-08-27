30a07216e8c9ec2c7a8cd71708cf41b337decdac
Vasanth <vasanthuralikrishnan@gmail.com>
2019-08-27 22:51:40 +0530

    login module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLoginComponent } from './app-login/app-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLoginComponent
   ],
   imports: [
     BrowserModule,
